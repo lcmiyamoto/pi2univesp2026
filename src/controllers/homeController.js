44bb12d60f306ca909a22c07561a2e69ad91626d
=luiz <=>
2026-08-26 10:31:16 -0300

    titulo

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,6 @@
 exports.paginaInicial = (req, res) => {
   res.render('index', {
-    titulo: 'Este será o título da página',
+    titulo: 'Projeto Integrador 2026',
     numeros: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
   });
   return;
