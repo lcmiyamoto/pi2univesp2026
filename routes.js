13b9bb271995dc235e3e49ea9a3062bae6962838
=luiz <=>
2026-08-27 15:21:57 -0300

    7-28-05-2026

--- routes.js
+++ routes.js
@@ -9,7 +9,7 @@ route.get('/', homeController.index);
 
 // Rotas de Login
 route.get('/login/index', loginController.index);
-
+route.get ('/login' , loginController.index);
 route.post('/login/register', loginController.register);
 
 module.exports = route;
