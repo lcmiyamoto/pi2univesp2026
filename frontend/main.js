923ba499552cbd1475fffb72c11a99886c66bb5d
=luiz <=>
2026-09-01 12:57:20 -0300

    3-01-09-2026

--- frontend/main.js
+++ frontend/main.js
@@ -1,7 +1,6 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import './assets/css/style.css';
 
 //Bootstrap
 import {Collapse, Dropdown} from 'bootstrap';
-
-import './assets/css/style.css';
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ const path = require('path'); // CommonJS
 
 module.exports = {
   mode: 'production',
-  entry: './frontend/main.js',
+  entry:  './frontend/main.js',
+
   output: {
     path: path.resolve(__dirname, 'public', 'assets', 'js'),
     filename: 'bundle.js'
